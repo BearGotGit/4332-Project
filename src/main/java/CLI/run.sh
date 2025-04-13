3a9748675d1f85cc172c380f37fbdea3ca0b862f
Albro3459 <brodsky.alex22@gmail.com>
2025-04-12 22:34:35 -0500

    Merge from brodsky branch

--- src/main/java/CLI/run.sh
+++ src/main/java/CLI/run.sh
@@ -3,4 +3,4 @@ PROJ=$(pwd)
 make clean; make
 clear
 # -cp is for classpath. This tells java to use the compiled files in the out dir
-java -cp out Main.CLI # or `make run``
+java -cp out CLI.Main.CLI # or `make run`

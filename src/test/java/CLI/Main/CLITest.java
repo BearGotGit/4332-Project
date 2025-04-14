95c539a65a9b356a4deacb3e154ba2eaeb818388
Berend Grandt <beraulndt@gmail.com>
2025-04-14 02:04:24 -0500

    Merged tests for 7-9

--- src/test/java/CLI/Main/CLITest.java
+++ src/test/java/CLI/Main/CLITest.java
@@ -1,5 +1,8 @@
 package CLI.Main;
 
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +13,9 @@ import CLI.Models.*;
 
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 
 public class CLITest {
@@ -483,5 +488,149 @@ void bookIsAvailable() {
 
 //    OPTION 4)
 
+
+//    OPTION 7-9
+
+
+    // Specification tests
+
+    @Test
+    void addMemberTestShouldSucceed() {
+        // Chooses Add Member and submits valid input
+
+        // Arrange
+        String name = "testName";
+        String email = "testEmail";
+
+        // Fake user input: 7 (add member), then member fields, then anything to continue
+        String userInput = String.join("\n",
+                "7",              // Select option 7 (Add Member)
+                name,
+                email,
+                "",                     // (Empty input to get to menu)
+                "10"                    // Exit (after adding book, immediately exit)
+        );
+
+        // Create CLI with fake input
+        cli = new CLI(new StringReader(userInput), mockedLibrary);
+
+        // Stub that library.addMember() returns a dummy Member
+        Member mockedMember = mock(Member.class);
+        when(mockedLibrary.addMember(anyString(), anyString())).thenReturn(mockedMember);
+
+        // Act
+        cli.run();
+
+        // Assert that addMember was called correctly and printMemberInfo was called
+        verify(mockedLibrary).addMember(name, email);
+        verify(mockedMember).printMemberInfo();
+    }
+
+    @Test
+    void revokeMembershipTestShouldSucceed() {
+        // Chooses Revoke Membership and submits an existing member to revoke
+
+        // Arrange
+        String memberID = "testMemberID";
+
+        // Fake user input: 8 (revoke membership), then memberID, then anything to continue
+        String userInput = String.join("\n",
+                "8",
+                memberID,
+                "",                     // (Empty input to get to menu)
+                "10"                    // Exit (after adding book, immediately exit)
+        );
+
+        // Create CLI with fake input
+        cli = new CLI(new StringReader(userInput), mockedLibrary);
+
+        // Stub that library.addMember() returns true
+        when(mockedLibrary.revokeMembership(anyString())).thenReturn(true);
+
+        // Act
+        cli.run();
+
+        // Assert that addMember was called correctly and printMemberInfo was called
+        verify(mockedLibrary).revokeMembership(memberID);
+    }
+
+    @Property
+    void viewAllMembersTestShouldSucceed(@ForAll @IntRange(min = 1, max = 20) int size) {
+        // Chooses View All Members, members exist, should return all members
+
+        // Arrange
+        List<Member> members = IntStream.range(0, size)
+                .mapToObj(i -> mock(Member.class))
+                .toList();
+
+        // Fake user input: 9 (View All Members), then anything to continue
+        String userInput = String.join("\n",
+                "9",
+                "",                     // (Empty input to get to menu)
+                "10"                    // Exit (after adding book, immediately exit)
+        );
+
+        // Create CLI with fake input
+        Library mockedLibrary = mock(Library.class); // jqwik cannot use @BeforeEach from junit
+        cli = new CLI(new StringReader(userInput), mockedLibrary);
+
+        // Stub that library.getAllMembers() returns my list of members
+        when(mockedLibrary.getAllMembers()).thenReturn(members);
+
+        // Act
+        cli.run();
+
+        // Assert that each member correctly called printMemberInfo
+        members.forEach(m -> verify(m).printMemberInfo());
+    }
+
+    // Structural test
+
+    @Test
+    void addMemberTestEmptyInputShouldFail() {
+        // Chooses Add Member and submits empty input
+
+        // Arrange
+        // Fake user input: 7 (add member), then empty member fields, then anything to continue
+        String userInput = String.join("\n",
+                "7",              // Select option 7 (Add Member)
+                "",
+                "",
+                "",                     // (Empty input to get to menu)
+                "10"                    // Exit (after adding book, immediately exit)
+        );
+
+        // Create CLI with fake input
+        cli = new CLI(new StringReader(userInput), mockedLibrary);
+
+        // Act
+        cli.run();
+
+        // Assert that addMember() was never called
+        verify(mockedLibrary, never()).addMember(anyString(), anyString());
+    }
+
+    @Test
+    void revokeMembershipTestShouldFail() {
+        // Chooses Revoke Membership and submits an empty member ID
+
+        // Arrange
+        // Fake user input: 8 (revoke membership), then memberID, then anything to continue
+        String userInput = String.join("\n",
+                "8",
+                "",
+                "",                     // (Empty input to get to menu)
+                "10"                    // Exit (after adding book, immediately exit)
+        );
+
+        // Create CLI with fake input
+        cli = new CLI(new StringReader(userInput), mockedLibrary);
+
+        // Act
+        cli.run();
+
+        // Assert that addMember was called correctly and printMemberInfo was called
+        verify(mockedLibrary, never()).revokeMembership(anyString());
+    }
 
 }

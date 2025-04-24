5344a17e02a9acd10326e2e612ad199412cc1363
Berend Grandt <beraulndt@gmail.com>
2025-04-23 22:27:43 -0500

    Librarians.java

--- src/main/java/CLI/Models/Librarians.java
+++ src/main/java/CLI/Models/Librarians.java
@@ -1,4 +1,24 @@
 package CLI.Models;
 
+import java.util.*;
+
 public class Librarians {
+
+    private Map<String, Integer> authCodes = new HashMap<>();
+    private Map<String, Float> withdraws = new HashMap<>();
+    private Map<String, List<Book>> purchases = new HashMap<>();
+
+    public Librarians() {
+        List<String> librarians = List.of(
+                "Ube",
+                "Wolfeschlegelsteinhausenbergerdorff",
+                "Ottovordemgentschenfelde"
+        );
+
+        for (String l : librarians) {
+            authCodes.put(l, 0);
+            withdraws.put(l, 0.0f);
+            purchases.put(l, new ArrayList<>());
+        }
+    }
 }

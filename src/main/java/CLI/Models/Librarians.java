fd07b6e48f9855a1a5655195f775d0e6ad3c1f00
Berend Grandt <beraulndt@gmail.com>
2025-04-23 22:33:52 -0500

    fixed auth code, now string

--- src/main/java/CLI/Models/Librarians.java
+++ src/main/java/CLI/Models/Librarians.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Librarians {
 
-    private Map<String, Integer> authCodes = new HashMap<>();
+    private Map<String, String> authCodes = new HashMap<>();
     private Map<String, Float> withdraws = new HashMap<>();
     private Map<String, List<Book>> purchases = new HashMap<>();
 
@@ -16,9 +16,21 @@ public class Librarians {
         );
 
         for (String l : librarians) {
-            authCodes.put(l, 0);
+            authCodes.put(l, makeAuthCode());
             withdraws.put(l, 0.0f);
             purchases.put(l, new ArrayList<>());
         }
+    }
+
+    private String makeAuthCode() {
+        Random rand = new Random();
+        String authCode = null;
+        while (authCode == null) {
+            authCode = rand.ints(6, 0,10).toString();
+            if (authCodes.containsValue(authCode)) {
+                authCode = null;
+            }
+        }
+        return authCode;
     }
 }
